2dc64be7a92ccdf679f99e6fa563b4d32d965d02
Yanan2236 <kent31415926535@gmail.com>
2025-08-10 03:39:18 +0900

    fix: add missing commma in INSTALLED_APPS

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'crud.apps.CrudConfig'
+    'crud.apps.CrudConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
