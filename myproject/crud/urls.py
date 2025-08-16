f4a7b06451465d52ae1e845c51f3aef74a99a973
Yanan2236 <kent31415926535@gmail.com>
2025-08-16 18:58:42 +0900

    feat: add product creation feature

--- myproject/crud/urls.py
+++ myproject/crud/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.ProductListView.as_view(), name="list"),
+    path("new/", views.ProductCreateView.as_view(), name="new"),
 ]
--- myproject/crud/views.py
+++ myproject/crud/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Product
+from django.urls import reverse_lazy
+from .forms import ProductForm
 
 class ProductListView(ListView):
     model = Product
     context_object_name = "products"
     paginate_by = 3
+
+class ProductCreateView(CreateView):
+    model = Product
+    form_class = ProductForm
+    success_url = reverse_lazy("list")
