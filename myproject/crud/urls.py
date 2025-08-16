a3ac7b2e59809239c32da021a225163b7e19e16d
Yanan2236 <kent31415926535@gmail.com>
2025-08-17 00:11:07 +0900

    feat: implement detail product

--- myproject/crud/urls.py
+++ myproject/crud/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.ProductListView.as_view(), name="list"),
     path("new/", views.ProductCreateView.as_view(), name="new"),
     path("edit/<int:pk>", views.ProductUpdateView.as_view(), name="edit"),
-    path("delete/<int:pk>", views.ProductDeleteView.as_view(), name="delete")
+    path("delete/<int:pk>", views.ProductDeleteView.as_view(), name="delete"),
+    path("detail/<int:pk>", views.ProductDetailView.as_view(), name="detail")
 ]
--- myproject/crud/views.py
+++ myproject/crud/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 from .models import Product
 from django.urls import reverse_lazy
 from .forms import ProductForm
@@ -24,3 +24,6 @@ class ProductUpdateView(UpdateView):
 class ProductDeleteView(DeleteView):
     model = Product
     success_url = reverse_lazy("list")
+
+class ProductDetailView(DetailView):
+    model = Product
