10cfdb9e8c3459560f94fdb0e1a0e35c59b0bcf9
Yanan2236 <kent31415926535@gmail.com>
2025-08-16 19:11:20 +0900

    refactor: add verbose_name for Product model

--- myproject/crud/models.py
+++ myproject/crud/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.urls import reverse
 
 class Product(models.Model):
-    name = models.CharField(max_length=200)
-    price = models.PositiveIntegerField()
+    name = models.CharField(max_length=200, verbose_name="商品")
+    price = models.PositiveIntegerField(verbose_name="価格")
     
     def __str__(self):
         return self.name
@@ -11,3 +11,8 @@ class Product(models.Model):
 
     def get_absolute_url(self):
         raise NotImplementedError("詳細ページ実装時に定義予定")
+    
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "アイテム"
+        verbose_name_plural = "商品一覧"
