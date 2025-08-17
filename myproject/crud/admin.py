7a64de698535f97c8b4e7cade252008e56dbf3ec
Yanan2236 <kent31415926535@gmail.com>
2025-08-17 16:24:31 +0900

    feat: add product img

--- myproject/crud/admin.py
+++ myproject/crud/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin 
 from .models import Product, Category
+from django.utils.safestring import mark_safe
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -8,6 +9,9 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price', 'category')
+    list_display = ('id', 'name', 'price', 'category', 'image')
     search_fields = ('name',)
     list_filter = ('category',)
+
+    def image(self, obj):
+        return mark_safe(f'<img src="{obj.img.url}" alt="商品画像" style="width:100px; height:auto;">')
--- myproject/crud/forms.py
+++ myproject/crud/forms.py
@@ -6,20 +6,24 @@ BASE_INPUT_CLS = "block w-full rounded border border-gray-300 px-3 py-2 text-gra
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ["name", "price", "category"]
+        fields = ["name", "price", "category", "img"]
         widgets = {
             "name": forms.TextInput(attrs={
-                "class": BASE_INPUT_CLS, "placeholder": "商品名"
+                "class": BASE_INPUT_CLS, "placeholder": "商品名",
             }),
             "price": forms.NumberInput(attrs={
-                "class": BASE_INPUT_CLS, "min": "0", "step": "1", "placeholder": "価格"
+                "class": BASE_INPUT_CLS, "min": "0", "step": "1", "placeholder": "価格",
             }),
+            "image": forms.ClearableFileInput(attrs={
+                "class": BASE_INPUT_CLS, "accept": "image/*",
+            }),            
             "category": forms.Select(attrs={
-                "class": BASE_INPUT_CLS, "placeholder": "カテゴリ"
-            })
+                "class": BASE_INPUT_CLS,
+            }),
         }
         labels = {
             "name": "商品名",
             "price": "価格",
-            "category": "カテゴリ"
+            "category": "カテゴリ",
+            "img": "商品画像",
         }
